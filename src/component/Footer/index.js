e2c8b91d20700aa26ad1f2eec93ab3cbb1e93e2e
snadi043 <snadi043@uottawa.ca>
2025-08-15 10:55:15 -0400

    Update index.js
    Refactored the code in the footer file to resolve the build errors in the build step of the workflow.

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -2,25 +2,33 @@ import './style.css';
 const Footer = () => {
     return (
         <footer>
-        <a href="https://github.com/snadi043/gitRevision" target="_blank">
-        <img
-          src=""
-          alt="GitHub-icon"
-        />
+        <a 
+            href="https://github.com/snadi043/MYPROFILE" 
+            target="_blank"
+            rel="noopener noreferrer">
+                <img
+                  src="https://camo.githubusercontent.com/17a3cfebe6cf2dcf7b339b7b008adb9a55ddc15aec622a27a2a66b207e1e357a/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4769744875622d3130303030303f7374796c653d666f722d7468652d6261646765266c6f676f3d676974687562266c6f676f436f6c6f723d7768697465"
+                  alt="GitHub-icon"
+                />
         </a>
 
-        <a href="https://www.facebook.com/DwinaTech" target="_blank">
-        <img
-          src="https://github.com/DwinaTech/public-images/blob/main/facebook-con.png?raw=true"
-          alt="YouTube-icon"
-        />
-
+        <a 
+            href="https://hub.docker.com/repositories/043sdi" 
+            target="_blank"
+            rel="noopener noreferrer">
+                <img
+                  src="https://camo.githubusercontent.com/4b2ee004dae7f21859f24d85941f79153219d2506a70b1a905fa31f97f4b5702/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f646f636b65722d3235376264363f7374796c653d666f722d7468652d6261646765266c6f676f3d646f636b6572266c6f676f436f6c6f723d7768697465"
+                  alt="Docker-icon"
+                />
         </a>
-        <a href="https://www.linkedin.com/in/sai-harnadh-28823a170/"  target="_blank">
-        <img
-          src="https://github.com/DwinaTech/public-images/blob/main/linkedin-icon.png?raw=true"
-          alt="LinkIn-icon"
-        />
+        <a 
+            href="https://www.linkedin.com/in/sai-harnadh-n-28823a170/"  
+            target="_blank"
+            rel="noopener noreferrer">
+                <img
+                  src="https://camo.githubusercontent.com/8c0692475a5bfc1d9e7361074bdb648e567cae7b5b40ffd32adae31180b0d7b6/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c696e6b6564496e2d3030373742353f7374796c653d666f722d7468652d6261646765266c6f676f3d6c696e6b6564696e266c6f676f436f6c6f723d7768697465"
+                  alt="LinkedIn-icon"
+                />
         </a>
         </footer>
     );
